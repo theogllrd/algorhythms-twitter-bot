a5bf1b334fb00eba5bca8acaa4f842b3032dbe87
Theo Gaillard <theogllrd@gmail.com>
2022-04-02 15:24:18 +0200

    testing upload of the video

--- tweet.js
+++ tweet.js
@@ -21,7 +21,7 @@ async function tweet(tweetText) {
 
     twitterClient.post('media/upload', media, (error, media, response) => {
         if (!error) {
-            console.log(`Successfully uploaded video: ${JSON.stringify(response)}`);
+            console.log(`Successfully uploaded video: ${JSON.stringify(response.headers)}`);
         } else {
             console.error(error);
         }
