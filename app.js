bbeee91f0a252e04ac0bea4f9016d00aeaf145f8
Theo Gaillard <theogllrd@gmail.com>
2022-04-05 16:36:09 +0200

    changing fromBlock value to tests with multiple tweets

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ async function monitorContract() {
   const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
 
   contract.events
-    .Transfer({fromBlock:14525093})
+    .Transfer({fromBlock:14480381})
     .on('connected', (subscriptionId) => {
       console.log(subscriptionId);
     })
