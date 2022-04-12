70751528c48df5a1d8e99ef8305fe165e341a1f7
Theo Gaillard <theogllrd@gmail.com>
2022-04-12 22:48:15 +0200

    removing fromBlock instruction

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ async function monitorContract() {
   const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
 
   contract.events
-    .Transfer({fromBlock: 14565956})
+    .Transfer({})
     .on('connected', (subscriptionId) => {
       console.log(subscriptionId);
     })
